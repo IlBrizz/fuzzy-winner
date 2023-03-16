aa351dd23740fb3d90ba2d537b7f7cf7ef8b5f13
IlBrizz <128077384+IlBrizz@users.noreply.github.com>
2023-03-16 21:13:55 +0100

    Update keyword_extractor.js

--- keyword_extractor.js
+++ keyword_extractor.js
@@ -1,56 +1,67 @@
-$(document).ready(function() {
+// Funzione per gestire la ricerca dei libri
+function searchBooks() {
+  // Ottieni la parola chiave inserita dall'utente
+  const keyword = document.getElementById('keyword').value.trim();
 
-  // Funzione per eseguire la ricerca dei libri
-  function searchBooks() {
-    var keyword = $("#keyword").val(); // recupera la parola chiave dalla textbox
-    var maxResults = $("#maxResults").val(); // recupera il numero massimo di risultati dalla textbox
-    var maxPrice = $("#maxPrice").val(); // recupera il prezzo massimo dalla textbox
+  // Controlla se l'utente ha inserito una parola chiave
+  if (!keyword) {
+    alert('Inserisci una parola chiave per la ricerca!');
+    return;
+  }
 
-    // Controlla se il prezzo massimo è valido e maggiore di zero
-    if (isNaN(maxPrice) || maxPrice <= 0) {
-      alert("Inserire un prezzo massimo valido");
-      return;
-    }
+  // Effettua la chiamata AJAX per ottenere i libri
+  $.ajax({
+    url: `https://api.myjson.com/bins/${jsonUrl}`,
+    dataType: 'json',
+    success: function(data) {
+      // Filtra i libri in base alla parola chiave
+      const filteredBooks = data.filter(book =>
+        book.title.toLowerCase().includes(keyword.toLowerCase())
+      );
 
-    // Effettua la chiamata AJAX per recuperare i dati dal server
-    $.ajax({
-      url: "/search_books",
-      type: "POST",
-      data: { keyword: keyword, max_results: maxResults, max_price: maxPrice },
-      dataType: "json",
-      success: function(data) {
-        // Aggiorna la tabella con i dati dei libri
-        updateTable(data);
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        alert("Errore durante la ricerca dei libri: " + textStatus);
+      // Filtra i libri in base agli altri criteri
+      const filteredBooks2 = filteredBooks.filter(book =>
+        book.resultCount < 800 &&
+        book.titleCount >= 6 &&
+        book.daysSincePublication >= 30 &&
+        book.maxBsrEbook < 80000 &&
+        book.maxBsrPaperback < 200000 &&
+        book.avgReviews <= 100 &&
+        book.maxPrice <= 14.90 &&
+        !book.famousAuthor &&
+        !book.trademark
+      );
+
+      // Se ci sono libri, mostra la tabella
+      if (filteredBooks2.length > 0) {
+        // Aggiungi i libri alla tabella
+        const tbody = document.querySelector('#results tbody');
+        tbody.innerHTML = '';
+        filteredBooks2.forEach(book => {
+          const row = document.createElement('tr');
+          row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.reviewCount}</td>
+            <td>${book.publicationDate}</td>
+            <td>${book.maxPrice}</td>
+            <td>${book.maxBsrEbook}</td>
+            <td>${book.maxBsrPaperback}</td>
+          `;
+          tbody.appendChild(row);
+        });
+
+        // Mostra la tabella
+        document.getElementById('results').classList.remove('d-none');
+        document.getElementById('no-results').classList.add('d-none');
+      } else {
+        // Nascondi la tabella e mostra il messaggio
+        document.getElementById('results').classList.add('d-none');
+        document.getElementById('no-results').classList.remove('d-none');
       }
-    });
-  }
-
-  // Funzione per aggiornare la tabella con i dati dei libri
-  function updateTable(data) {
-    var table = $("#bookTable tbody");
-    table.empty(); // svuota la tabella precedente
-
-    // Aggiunge i dati di ogni libro alla tabella
-    for (var i = 0; i < data.length; i++) {
-      var book = data[i];
-      var row = $("<tr>");
-      row.append($("<td>").text(book.title));
-      row.append($("<td>").text(book.author));
-      row.append($("<td>").text(book.num_reviews));
-      row.append($("<td>").text(book.publication_date));
-      row.append($("<td>").text(book.price));
-      row.append($("<td>").text(book.bsr));
-      table.append(row);
+    },
+    error: function() {
+      alert('Errore durante il recupero dei dati!');
     }
-
-    // Mostra la tabella
-    $("#bookTable").show();
-  }
-
-  // Aggiungi l'evento click al pulsante di ricerca
-  $("#searchBtn").click(searchBooks);
-
-});
+  });
+}
